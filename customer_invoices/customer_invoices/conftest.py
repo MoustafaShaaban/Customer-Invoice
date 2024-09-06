023605f3abbb11c0c61abc854f22eb2ac6788163
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2024-09-06 17:39:09 +0300

    Started Cookiecutter Django template

---
+++ customer_invoices/customer_invoices/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from customer_invoices.users.models import User
+from customer_invoices.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ customer_invoices/customer_invoices/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from customer_invoices.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
